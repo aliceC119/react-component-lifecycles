ac4835962013d08c3a1317b73b659918026e7623
Alice Chan <alicebay10@yahoo.co.uk>
2024-12-10 13:12:38 +0000

    Created LifeCyclesCDM.js & LifeCyclesCDMChild.js for the lesson of CompnentDidMount.

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import LifeCyclesCDM from "./components/LifeCyclesCDM";
+
+function App() {
+  return <div className="App">
+      <LifeCyclesCDM />
+  </div>;
+}
+
+export default App;
